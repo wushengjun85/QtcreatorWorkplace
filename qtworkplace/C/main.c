fac6b3422b792a016a26afdb608e65e8eb3b5f1d
吴胜军 <wushengjun85@163.com>
2017-05-19 11:12:08 +0800

    double型占用字节数

--- qtworkplace/C/main.c
+++ qtworkplace/C/main.c
@@ -4,5 +4,6 @@ int main(int argc, char *argv[])
 {
     printf("Hello World!\n");
     printf("sizeof(float) == %d\r\n",sizeof(float));
+    printf("sizeof(double) == %d\r\n",sizeof(double));
     return 0;
 }
