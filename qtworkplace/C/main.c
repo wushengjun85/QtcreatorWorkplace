d9c1605b10ac43ecc6a3fb9d7ded6317e8e1b4c6
吴胜军 <wushengjun85@163.com>
2017-05-19 11:14:58 +0800

    long型占用字节数

--- qtworkplace/C/main.c
+++ qtworkplace/C/main.c
@@ -5,5 +5,6 @@ int main(int argc, char *argv[])
     printf("Hello World!\n");
     printf("sizeof(float) == %d\r\n",sizeof(float));
     printf("sizeof(double) == %d\r\n",sizeof(double));
+    printf("sizeof(long) == %d\r\n",sizeof(long));
     return 0;
 }
