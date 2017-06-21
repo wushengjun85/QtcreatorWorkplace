97bba222b83edb5fb8a9a1e32cc32498b15ab832
吴胜军 <wushengjun85@163.com>
2017-06-21 12:02:44 +0800

    update

---
+++ qtworkplace/LabelClicked/mytestlabel.cpp
@@ -0,0 +1,6 @@
+#include "mytestlabel.h"
+
+MytestLabel::MytestLabel()
+{
+
+}
---
+++ qtworkplace/LabelClicked/mytestlabel.h
@@ -0,0 +1,17 @@
+#ifndef MYTESTLABEL_H
+#define MYTESTLABEL_H
+#include<QLabel>
+
+#include <QObject>
+#include <QWidget>
+
+class MytestLabel : public QLabel
+{
+public:
+    MytestLabel();
+
+signals:
+
+};
+
+#endif // MYTESTLABEL_H
