756ff1685c3bcbab2852276c5588c9c794c891db
吴胜军 <wushengjun85@163.com>
2017-07-05 18:14:54 +0800

    commit

--- qtworkplace/Testone/widget.cpp
+++ qtworkplace/Testone/widget.cpp
@@ -1,6 +1,7 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include<QHBoxLayout>
+#include<QVBoxLayout>
 #include<QSpinBox>
 #include<QPushButton>
 Widget::Widget(QWidget *parent) :
@@ -8,7 +9,7 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
-
+#if 1
    // QWidget *window = new QWidget;
     QPushButton *button1 = new QPushButton("One");
     QPushButton *button2 = new QPushButton("Two");
@@ -16,17 +17,35 @@ Widget::Widget(QWidget *parent) :
     QPushButton *button4 = new QPushButton("Four");
     QPushButton *button5 = new QPushButton("Five");
 
-    QHBoxLayout *layout = new QHBoxLayout;
-    layout->addWidget(button1);
-    layout->addWidget(button2);
-    layout->addWidget(button3);
-    layout->addWidget(button4);
-    layout->addWidget(button5);
+//    QVBoxLayout *layout = new QVBoxLayout;
+//    layout->addWidget(button1);
+//    layout->addWidget(button2);
+//    layout->addWidget(button3);
+//    layout->addWidget(button4);
+//    layout->addWidget(button5);
+
+    QGridLayout *layout = new QGridLayout;
+    layout->addWidget(button1, 0, 0);
+    layout->addWidget(button2, 0, 1);
+    layout->addWidget(button3, 1, 0, 1, 2);
+    layout->addWidget(button4, 2, 0);
+    layout->addWidget(button5, 2, 1);
 
 //    window->setLayout(layout);
 //    window->show();
     this->setLayout(layout);
     this->show();
+#endif
+
+#if 0
+        QVBoxLayout *layout = new QVBoxLayout;
+        layout->addWidget(ui->lineEdit);
+        layout->addWidget(ui->lineEdit_2);
+        this->setLayout(layout);
+        this->show();
+#endif
+
+
 }
 
 Widget::~Widget()
